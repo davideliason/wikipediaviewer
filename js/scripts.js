8212e86d89934aa9cc046af84c6843b57e63914d
David Eliason <davethemaker@gmail.com>
2016-03-22 12:11:21 -0700

    add AJAX call using jQuery to grab example code from Wikipedia API

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,23 @@ $(document).ready(function(){
 	});
 
 	$("#searchWikipedia").on("click",function(){
-		alert("search");
-	});
+		alert("button2 clicked");
+
+	 $.ajax({
+        type: "GET",
+        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+            console.log(data);
+        },
+        error: function (errorMessage) {
+        }
+    });
+
+
+   });
 
 
 });
