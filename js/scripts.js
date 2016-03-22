79c54511eb7043c60fb31db59631d7390fdec85f
David Eliason <davethemaker@gmail.com>
2016-03-22 11:03:27 -0700

    set background image using z-index and wikipedia data to sit on top of that

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,3 @@
 $(document).ready(function(){
-
-  <!-- HTML5 Geocaching -->
-
- if (navigator.geolocation) {
-   navigator.geolocation.getCurrentPosition(function(position) {
-   var latitude = position.coords.latitude;
-   var longitude = position.coords.longitude;
-
-   console.log(longitude);
-  });
-}
-
+	console.log("jQuery working");
 });
