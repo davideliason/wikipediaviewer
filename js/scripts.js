d9813475cdc7190875e86ab008878c173134c55e
David Eliason <davethemaker@gmail.com>
2016-03-22 19:19:39 -0700

    place GO button next to query data input field; jQuery to grab value of input field

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,27 @@ $(document).ready(function(){
 	$("#searchWikipedia").on("click",function(){
 
 		$("#search_box").show();
-			
-    	$.ajax({
-        type: "GET",
-        url: "http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=Albert%20Einstein&callback=?",
-        contentType: "application/json; charset=utf-8",
-        async: false,
-        dataType: "json",
-        success: function (data, textStatus, jqXHR) {
-            console.log(data.query[search][0]);
-        },
-        error: function (errorMessage) {
-        }
-    });
+
+		$("#submitQuery").on("click",function(){
+			var queryData = $("#searchCriteria").val();
+			alert(queryData);
+
+			$.ajax({
+		        type: "GET",
+		        url: "http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=Albert%20Einstein&callback=?",
+		        contentType: "application/json; charset=utf-8",
+		        async: false,
+		        dataType: "json",
+		        success: function (data, textStatus, jqXHR) {
+		            console.log(data);
+		        },
+		        error: function (errorMessage) {
+        		}
+    		});
+
+		});
+		
+    	
     
     // $.getJSON("http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?", {page:"Red Sea clownfish", prop:"text"}, function(data) {console.log(data);});
 
