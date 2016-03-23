e07e630bb654da67304436d8354f73f7b9d7e633
David Eliason <davethemaker@gmail.com>
2016-03-22 18:13:24 -0700

    add input text area

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,23 @@ $(document).ready(function(){
 	});
 
 	$("#searchWikipedia").on("click",function(){
-		alert("button2 clicked");
 
-	 $.ajax({
+			
+    $.ajax({
         type: "GET",
-        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Battlestar_Galactica&callback=?",
+        url: "http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=Albert%20Einstein&callback=?",
         contentType: "application/json; charset=utf-8",
         async: false,
         dataType: "json",
         success: function (data, textStatus, jqXHR) {
-            console.log(data);
+            console.log(data.query[search][0]);
         },
         error: function (errorMessage) {
         }
     });
+    
+    // $.getJSON("http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?", {page:"Red Sea clownfish", prop:"text"}, function(data) {console.log(data);});
+
 
 
    });
